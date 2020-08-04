1912aaae98d5a8388675fe893bd04fe505908370
Somdip Sen <38436523+Somdip-Sen@users.noreply.github.com>
2020-08-04 16:39:04 +0530

    Update tic_tac_toe.py
    fixed bugs
    multiple playing options added

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,38 @@
 import pygame
+import time
 
 x_, y_ = None, None
 turn = 1
 win = False
-tik_tak_toe = x = [[3 for i in range(3)] for j in range(3)]  # fill matrix with default garbage value 3
+tik_tak_toe = [[3 for i in range(3)] for j in range(3)]  # fill matrix with default garbage value 3
+
+
+def victory_line(matching_type, pattern):  # type 1 -> row wise match | 2-> column wise match | 3 -> Diagonal match
+    if matching_type == 1:  # pattern -> 0,1 or 2 => which row
+        pygame.draw.line(canvas, (255, 0, 0), (10, pattern * 120 + 60), (340, pattern * 120 + 60), 3)
+        pygame.display.flip()
+        time.sleep(0.8)
+    elif matching_type == 2:  # pattern -> 0,1 or 2 => which column
+        pygame.draw.line(canvas, (255, 0, 0), (pattern * 120 + 60, 10), (pattern * 120 + 60, 340), 3)
+        pygame.display.flip()
+        time.sleep(0.8)
+    else:  # pattern -> 1 or 2 => which Diagonal
+        if pattern == 1:  # left to right
+            pygame.draw.line(canvas, (255, 0, 0), (10, 10), (340, 340), 3)
+            pygame.display.flip()
+            time.sleep(0.8)
+        else:  # right to left
+            pygame.draw.line(canvas, (255, 0, 0), (340, 10), (10, 340), 3)
+            pygame.display.flip()
+            time.sleep(0.8)
+
+
+def reset(arr):
+    for i, e in enumerate(arr):
+        if isinstance(e, list):
+            reset(e)
+        else:
+            arr[i] = 3
 
 
 def show_move(x, y):
@@ -22,17 +51,22 @@ def is_win():
     for i in range(3):
         # row wise check
         if tik_tak_toe[i][0] != 3 and all(elem == tik_tak_toe[i][0] for elem in tik_tak_toe[i]):
+            victory_line(1, i)
             return tik_tak_toe[i][0]
         # column wise check
         if tik_tak_toe[0][i] != 3:
             if all(tik_tak_toe[j][i] == tik_tak_toe[0][i] for j in range(3)):
+                victory_line(2, i)
                 return tik_tak_toe[0][i]
     # diagonal
     if tik_tak_toe[0][0] != 3 and all(
             tik_tak_toe[j][j] == tik_tak_toe[0][0] for j in range(3)):  # left to right diagonal
+        time.sleep(0.3)
+        victory_line(3, 1)
         return tik_tak_toe[0][0]
     if tik_tak_toe[0][2] != 3 and all(
             tik_tak_toe[2 - j][j] == tik_tak_toe[0][2] for j in range(3)):  # right to left diagonal
+        victory_line(3, 2)
         return tik_tak_toe[0][2]
     for i in range(3):
         for j in range(3):
@@ -76,25 +110,32 @@ def playGUI():
         while win:
             canvas.fill((49, 150, 100))
             if win == "Draw":
-                canvas.blit(font1.render("Players Draw", True, (255, 255, 255)), (50, 100))
+                canvas.blit(font1.render("Players Draw", True, (255, 255, 255)), (25, 100))
             else:
-                canvas.blit(font1.render("Player " + str(win) + " Wins", True, (255, 255, 255)), (50, 100))
-            canvas.blit(font.render("Click to Exit ...", True, (255, 255, 255)), (110, 190))
+                canvas.blit(font1.render("Player " + str(win) + " Wins", True, (255, 255, 255)), (25, 100))
+            canvas.blit(font.render("Click to Exit ...", True, (255, 255, 255)), (130, 190))
+            canvas.blit(font.render("PRESS SPACE TO PLAY AGAIN...", True, (255, 255, 255)), (80, 230))
             event = pygame.event.wait()
-            if event.type == pygame.MOUSEBUTTONDOWN:  # Exit the match
+            if event.type == pygame.QUIT:  # Exit the window
                 pygame.quit()
                 exit()
-            if event.type == pygame.QUIT:
+            elif event.type == pygame.MOUSEBUTTONDOWN:  # Exit the match
                 pygame.quit()
                 exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    reset(tik_tak_toe)
+                    canvas.fill((49, 150, 100))
+                    break
             pygame.display.flip()
         pygame.display.flip()  # update the whole screen
 
 
 if __name__ == "__main__":
     pygame.init()  # initialize
-    font = pygame.font.SysFont('arial rounded mt bold', 30)
-    font1 = pygame.font.SysFont('game_over.ttf', 60)
+    font = pygame.font.SysFont('arial rounded mt bold', 20)
+    font_ = pygame.font.SysFont('arial', 10)
+    font1 = pygame.font.Font('game_over.ttf', 120)
     pygame.display.set_caption("TicTacToe")  # sets the window title
     canvas = pygame.display.set_mode((360, 360))  # sets the window size
     playGUI()
